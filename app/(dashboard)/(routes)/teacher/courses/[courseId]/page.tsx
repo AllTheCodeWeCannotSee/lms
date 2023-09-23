52d6944eb6980af8f65b3e769dac8fcb7a36d5f7
AllTheCodeWeCannotSee <183735517@qq.com>
2023-09-24 07:42:42 +0800

    course-category-form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -7,6 +7,7 @@ import { IconBadge } from "@/components/icon-badge";
 import {TitleForm} from "@/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form";
 import {DescriptionForm} from "@/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/description-form";
 import {ImageForm} from "@/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/image-form";
+import {CategoryForm} from "@/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/category-form";
 
 const CourseIdPage = async ({
     params
@@ -23,6 +24,12 @@ const CourseIdPage = async ({
         where: {
             id: params.courseId
         }
+    });
+
+    const categories = await db.category.findMany({
+        orderBy: {
+            name: "asc",
+        },
     });
 
     if (!course) {
@@ -75,6 +82,14 @@ const CourseIdPage = async ({
                     <ImageForm
                         initialData={course}
                         courseId={course.id}
+                    />
+                    <CategoryForm
+                        initialData={course}
+                        courseId={course.id}
+                        options={categories.map((category) => ({
+                            label:category.name,
+                            value: category.id,
+                        }))}
                     />
                 </div>
             </div>
