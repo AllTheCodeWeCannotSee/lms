75e3436c7ef0f441e7bb333623b5f9067dee5277
AllTheCodeWeCannotSee <183735517@qq.com>
2023-09-27 11:58:20 +0800

    categories-query & search-query & course-grid & card-ui

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,39 +1,48 @@
 "use client"
 
 import {LogOut} from "lucide-react";
-import { usePathname } from "next/navigation";
+import {usePathname} from "next/navigation";
 import Link from "next/link";
 
 import {UserButton} from "@clerk/nextjs";
 import {Button} from "@/components/ui/button";
+import {SearchInput} from "@/components/search-input";
 
 export const NavbarRoutes = () => {
     const pathname = usePathname();
 
     const isTeacherPage = pathname?.startsWith("/teacher");
     const isPlayerPage = pathname?.includes("/chapter");
+    const isSearchPage = pathname === "/search"
 
 
     return (
-        <div className="flex gap-x-2 ml-auto">
-            {isTeacherPage || isPlayerPage ? (
-                <Link href="/">
-                    <Button size="sm" variant="ghost">
-                        <LogOut className="h-4 w-4 mr-2"/>
-                        Exit
-                    </Button>
-                </Link>
-
-            ) : (
-                <Link href="/teacher/courses">
-                    <Button size="sm" variant="ghost">
-                        Teacher mode
-                    </Button>
-                </Link>
+        <>
+            {isSearchPage && (
+                <div className="hidden md:block">
+                    <SearchInput />
+                </div>
             )}
-            <UserButton
-                afterSignOutUrl="/"
-            />
-        </div>
+            <div className="flex gap-x-2 ml-auto">
+                {isTeacherPage || isPlayerPage ? (
+                    <Link href="/">
+                        <Button size="sm" variant="ghost">
+                            <LogOut className="h-4 w-4 mr-2"/>
+                            Exit
+                        </Button>
+                    </Link>
+
+                ) : (
+                    <Link href="/teacher/courses">
+                        <Button size="sm" variant="ghost">
+                            Teacher mode
+                        </Button>
+                    </Link>
+                )}
+                <UserButton
+                    afterSignOutUrl="/"
+                />
+            </div>
+        </>
     )
 }
